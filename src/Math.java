dfa34ba9cfbc2d0c3cb0a2372c156cffcaf57c69
sarahelenz <selenz3112@stu.d214.org>
2021-11-23 13:26:18 -0600

    Initial commit

---
+++ src/Math.java
@@ -0,0 +1,16 @@
+public class Math {
+    public static double E = 2.71828;
+    public static double PI = 3.14159;
+
+    public static double max(double a, double b){
+        if(a > b){
+            return a;
+        }else {
+            return b;
+        }
+    }
+    public static double min(double a, double b){
+        
+    }
+
+}
---
+++ src/test.java
@@ -0,0 +1,5 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println(Math.max(5,6));
+    }
+}
